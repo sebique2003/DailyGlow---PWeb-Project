01eb38123928fcb7bee8310189f7b7c8bf48e00e
Sebi Iordache <isebiiii09@gmail.com>
2025-03-26 18:22:24 +0200

    Chart.js - de facut

--- js/forms.js
+++ js/forms.js
@@ -1,9 +1,10 @@
-// Transition Signup-Login
+// Tranzitie intre Log In - Sign Up
 const loginBtn = document.getElementById('login-btn');
 const signupBtn = document.getElementById('signup-btn');
 const loginSection = document.getElementById('login-section');
 const signupSection = document.getElementById('signup-section');
 
+// Event pentru btn de Log In
 loginBtn.addEventListener('click', () => {
     signupSection.style.display = 'none';
     loginSection.style.display = 'block';
@@ -12,6 +13,7 @@ loginBtn.addEventListener('click', () => {
     }, 10);
 });
 
+// Event pentru btn de Sign Up
 signupBtn.addEventListener('click', () => {
     loginSection.style.display = 'none';
     signupSection.style.display = 'block';
